febb68c42980cf5ae259ec4ea5fa0b13343bb372
u1082615 <arpita.sadhukhan@iqvia.com>
2023-01-19 13:22:30 +0530

    First commit

---
+++ src/main/java/com/org/SigmaScript/SigmaScriptApplication.java
@@ -0,0 +1,13 @@
+package com.org.SigmaScript;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigmaScriptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigmaScriptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/org/SigmaScript/SigmaScriptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.SigmaScript;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigmaScriptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
